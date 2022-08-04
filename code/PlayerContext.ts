bc3291e9e786a7d9f718067c269ce1e0c5bb2fde
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2022-08-04 23:38:36 +0200

    Add linking objects to players (server only)

--- code/PlayerContext.ts
+++ code/PlayerContext.ts
@@ -1,6 +1,28 @@
 let currentPlayerID: string = "";
+const playerObjectsLists = new Map<string, gdjs.LongLivedObjectsList>();
 
 export const getCurrentPlayerID = () => currentPlayerID;
 export const switchPlayerContext = (playerID: string) => {
   currentPlayerID = playerID;
+};
+export const markObjectAsOwned = (object: gdjs.RuntimeObject) => {
+  let lists = playerObjectsLists.get(currentPlayerID);
+  if (!lists)
+    playerObjectsLists.set(
+      currentPlayerID,
+      (lists = new gdjs.LongLivedObjectsList())
+    );
+
+  lists.addObject(object.getName(), object);
+};
+
+export const pickOwnedObjects = (objectsListsHashtable: ObjectsLists) => {
+  const playerLists = playerObjectsLists.get(currentPlayerID);
+  if (!playerLists) return;
+
+  for (const [objectName, objectList] of Object.entries(
+    objectsListsHashtable.items
+  )) {
+    gdjs.copyArray(playerLists.getObjects(objectName), objectList);
+  }
 };
