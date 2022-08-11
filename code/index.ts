d3d53f86d5795ff26ac90bb4ab51e464bed7e89b
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2022-08-11 17:15:05 +0200

    Add support for syncing BBText and BitmapText

--- code/index.ts
+++ code/index.ts
@@ -3,3 +3,19 @@ export * as client from "./client";
 export { ClientAdapter, ServerAdapter } from "./Adapter";
 export * as settings from "./Settings";
 export * as players from "./PlayerContext";
+
+// A sort of polyfill to unify the different text objects get/set APIs.
+{
+  if (gdjs.BBTextRuntimeObject) {
+    gdjs.BBTextRuntimeObject.prototype.getString =
+      gdjs.BBTextRuntimeObject.prototype.getBBText;
+    gdjs.BBTextRuntimeObject.prototype.setString =
+      gdjs.BBTextRuntimeObject.prototype.setBBText;
+  }
+  if (gdjs.BitmapTextRuntimeObject) {
+    gdjs.BitmapTextRuntimeObject.prototype.getString =
+      gdjs.BitmapTextRuntimeObject.prototype.getText;
+    gdjs.BitmapTextRuntimeObject.prototype.setString =
+      gdjs.BitmapTextRuntimeObject.prototype.setText;
+  }
+}
