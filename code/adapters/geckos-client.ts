80ca6b7a7b4ad70b6f49c35fbc68546a00bc8e69
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2022-09-19 09:52:25 +0000

    Add prototype geckos.io adapter

---
+++ code/adapters/geckos-client.ts
@@ -0,0 +1,43 @@
+/// <reference path="../global.d.ts"/>
+import { geckos, type ClientChannel } from "@geckos.io/client";
+const logger = new gdjs.Logger("THNK - P2P Adapter");
+
+export class GeckosClientAdapter extends THNK.ClientAdapter {
+  ip: string;
+  port: number;
+  connection: ClientChannel | null = null;
+  constructor(ip: string, port: number) {
+    super();
+    this.ip = ip;
+    this.port = port;
+  }
+
+  async prepare(): Promise<void> {
+    this.connection = geckos({ url: this.ip, port: this.port, label: "THNK" });
+    await new Promise<void>((resolve, reject) =>
+      this.connection!.onConnect((error) => {
+        if (error) return reject(error.message);
+        this.connection!.onRaw((message) =>
+          this.onMessage(message as Uint8Array)
+        );
+        this.connection!.onDisconnect(() => this.onDisconnection());
+        resolve();
+      })
+    );
+  }
+
+  close() {
+    if (this.connection) this.connection.close();
+  }
+
+  protected doSendMessage(message: Uint8Array): void {
+    if (!this.connection) {
+      return logger.error(
+        "Tried to send a message on an unestablished connection!"
+      );
+    }
+    this.connection.raw.emit(
+      message.buffer.slice(message.buffer.byteLength - message.byteLength)
+    );
+  }
+}
