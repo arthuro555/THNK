c552aab2ea43a4e88e872edf0550e300ea620c47
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2023-01-12 19:51:07 +0100

    Rework the relay

--- code/relay/index.ts
+++ code/relay/index.ts
@@ -1,46 +1,113 @@
-import { geckos, ServerChannel } from "@geckos.io/server";
+import { geckos, ServerChannel as _ServerChannel } from "@geckos.io/server";
+import * as z from "zod";
 
 interface Room {
+  gameID: string;
   roomID: string;
-  server: ServerChannel;
-  clients: Set<ServerChannel>;
+  server?: ServerChannel;
+  clients: Map<string, ServerChannel>;
 }
 
 type Rooms = Map<string, Room>;
 
 interface UserData {
-  roomsForCurrentGame: Rooms;
-  currentRoom?: Room;
+  room: Room;
+  server: boolean;
 }
 
-interface AuthHeader {
-  // The game ID is used to make each game have their own set of rooms (game 1 cannot connect to a room of game 2).
-  gameID: string;
-  // Currently unused, but will be used to add presence and social features in the future.
-  liluoToken: string;
+interface ServerChannel extends _ServerChannel {
+  userData: UserData;
 }
+
+const authHeaderValidator = z.object({
+  type: z.union([z.literal("server"), z.literal("client")]),
+  gameID: z.string(),
+  roomID: z.string(),
+});
+
+export type AuthHeader = z.infer<typeof authHeaderValidator>;
 
 const roomsPerGame = new Map<string, Rooms>();
+function getRoomsForGame(gameID: string) {
+  let rooms = roomsPerGame.get(gameID);
+  if (!rooms) roomsPerGame.set(gameID, (rooms = new Map()));
+  return rooms;
+}
 
-function closeRoom(room: Room) {
-  // Disconnect all clients...
-  for (const _clientConnection of room.clients.values()) {
-    const clientConnection: Omit<ServerChannel, "userData"> & {
-      userData: UserData;
-    } = _clientConnection;
+function claimRoom(gameID: string, roomID: string): UserData {
+  const rooms = getRoomsForGame(gameID);
 
-    delete clientConnection.userData.currentRoom;
+  if (rooms.has(roomID)) {
+    throw "The requested room ID has already been claimed!";
   }
 
-  // Disconnect the server...
-  const serverConnection: Omit<ServerChannel, "userData"> & {
-    userData: UserData;
-  } = room.server;
+  const newRoom: Room = {
+    gameID,
+    roomID,
+    clients: new Map(),
+  };
 
-  delete serverConnection.userData.currentRoom;
+  rooms.set(roomID, newRoom);
 
-  // ...and finally, unclaim the room.
-  serverConnection.userData.roomsForCurrentGame.delete(room.roomID);
+  return { room: newRoom, server: true };
+}
+
+function joinRoom(gameID: string, roomID: string): UserData {
+  const rooms = getRoomsForGame(gameID);
+  const room = rooms.get(roomID);
+
+  if (!room) {
+    throw "The requested room does not exist!";
+  }
+
+  return { room, server: false };
+}
+
+function setupServer(server: ServerChannel) {
+  const { room } = server.userData;
+  room.server = server;
+
+  server.on("msg", ({ to, data }: any) => {
+    room.clients.get(to)?.raw.emit(data);
+  });
+
+  server.onDisconnect(() => closeRoom(room));
+}
+
+function setupClient(client: ServerChannel) {
+  const { room } = client.userData;
+
+  room.clients.set(client.id!, client);
+  room.server?.emit("connect", { userID: client.id! });
+
+  client.onRaw((rawMessage) => {
+    // The message came from a client. We must sadly add the source of the message, hence why we do not use a raw message.
+    // TODO: invetigate cost of doing this vs cost of copying the message to a new buffer of length n + 1
+    //       to get an extra slot where to store the connection ID + the cost of slicing the message back
+    //       into connection ID and message.
+    room.server?.emit("msg", { from: client.id!, data: rawMessage });
+  });
+
+  client.onDisconnect(() => {
+    room.server?.emit("disconnect", { userID: client.id! });
+    leaveRoom(room, client);
+  });
+}
+
+function leaveRoom(room: Room, client: ServerChannel): void {
+  room.clients.delete(client.id!);
+  client.close();
+}
+
+function closeRoom(room: Room): void {
+  for (const clientConnection of room.clients.values())
+    clientConnection.close();
+
+  room.server?.close();
+
+  // Unclaim the room.
+  const gameRooms = roomsPerGame.get(room.gameID);
+  if (gameRooms) gameRooms.delete(room.roomID);
 }
 
 const server = geckos({
@@ -51,89 +118,24 @@ const server = geckos({
   portRange: { min: 9696, max: 9696 },
   // We obtain the game ID in advance, to know to what game rooms from that connection will belong.
   async authorization(auth): Promise<UserData | false> {
-    if (!auth) return false;
-    try {
-      var { gameID } = JSON.parse(auth) as AuthHeader;
-    } catch (e) {
-      console.error(
-        "A connection has been blocked: Auth header is invalid JSON."
-      );
-      return false;
-    }
+    const { type, gameID, roomID } = authHeaderValidator.parse(auth);
+    if (type === "server") return claimRoom(gameID, roomID);
+    if (type === "client") return joinRoom(gameID, roomID);
 
-    let roomsForCurrentGame = roomsPerGame.get(gameID);
-    // If this game doesn't has a rooms map already, create one.
-    if (!roomsForCurrentGame)
-      roomsPerGame.set(gameID, (roomsForCurrentGame = new Map()));
-
-    // We persist the rooms map of the current game to user data to get rid of costly map lookups when connecting to a room
-    return { roomsForCurrentGame };
+    // Should never happen thanks to zod, but
+    return false;
   },
 });
 
 server.onConnection((_connection) => {
-  // Override type information to contain the correct user data type
-  const connection: Omit<ServerChannel, "userData"> & { userData: UserData } =
-    _connection;
+  // Override type information to contain the correct userData type
+  const connection = _connection as ServerChannel;
 
-  connection.on("claim", (roomID) => {
-    if (typeof roomID !== "string") return;
-    if (connection.userData.roomsForCurrentGame.has(roomID)) {
-      connection.emit(
-        "error",
-        "The requested room ID has already been claimed!"
-      );
-      return;
-    }
-
-    if (connection.userData.currentRoom)
-      closeRoom(connection.userData.currentRoom);
-
-    const newRoom: Room = {
-      roomID,
-      // Use the unmodified connection type here to workaround bad typings
-      server: _connection,
-      clients: new Set(),
-    };
-
-    connection.userData.roomsForCurrentGame.set(roomID, newRoom);
-    connection.userData.currentRoom = newRoom;
-  });
-
-  connection.on("join", (roomID) => {
-    if (typeof roomID !== "string") return;
-    const room = connection.userData.roomsForCurrentGame.get(roomID);
-    if (!room) return;
-    if (connection.userData.currentRoom)
-      closeRoom(connection.userData.currentRoom);
-
-    // Use the unmodified connection type here to workaround bad typings
-    room.clients.add(_connection);
-    connection.userData.currentRoom = room;
-  });
-
-  connection.onRaw((rawMessage) => {
-    const room = connection.userData.currentRoom;
-    if (!room) return;
-    if (room.server === connection) {
-      // The message comes from the registered host: forward to all clients directly.
-      for (const client of room.clients.values()) {
-        client.raw.emit(rawMessage);
-      }
-    } else {
-      // The message came from a client. We must sadly add the source of the message, hence why we do not use a raw message.
-      // TODO: invetigate cost of doing this vs cost of copying the message to a new buffer of length n + 1
-      //       to get an extra slot where to store the connection ID + the cost of slicing the message back
-      //       into connection ID and message.
-      server.emit("msg", { from: connection.id, message: rawMessage });
-    }
-  });
-
-  connection.onDisconnect(() => {
-    // If this client had a claimed room, unclaim it.
-    if (connection.userData.currentRoom)
-      closeRoom(connection.userData.currentRoom);
-  });
+  if (connection.userData.server) {
+    setupServer(connection);
+  } else {
+    setupClient(connection);
+  }
 });
 
 // The signaling server must run on port 6969, per convention.
--- code/types/global.d.ts
+++ code/types/global.d.ts
@@ -1,9 +1,17 @@
 export * from "..";
 import type { ClientAdapter, ServerAdapter } from "adapters/Adapter";
+
 export class GeckosClientAdapter extends ClientAdapter {
   constructor(ip: string, port: number) {}
 }
 export class GeckosServerAdapter extends ServerAdapter {
   constructor(port: number) {}
 }
+export class RelayClientAdapter extends ClientAdapter {
+  constructor(gameID: string, roomID: string) {}
+}
+export class RelayServerAdapter extends ServerAdapter {
+  constructor(gameID: string, roomID: string) {}
+}
+
 export as namespace THNK;
