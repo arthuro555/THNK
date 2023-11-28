e732f74af7e6f5ec72a9bceafd315806543a26fe
Arthur Pacaud <arthur.pacaud@hotmail.fr>
2023-11-28 15:58:13 +0100

    Fix Bitmap Text String not being synced

--- code/index.ts
+++ code/index.ts
@@ -5,21 +5,26 @@ export * as settings from "utils/Settings";
 export * as players from "server/PlayerContext";
 
 // A sort of polyfill to unify the different text objects get/set APIs.
-{
-  if (gdjs.BBTextRuntimeObject) {
-    gdjs.BBTextRuntimeObject.prototype.getString =
-      gdjs.BBTextRuntimeObject.prototype.getBBText;
-    gdjs.BBTextRuntimeObject.prototype.setString =
-      gdjs.BBTextRuntimeObject.prototype.setBBText;
+// Since THNK loads as GDJS files are still loading, the callback is 
+// necessary to ensure the polyfill is ran once the objects classes
+// are loaded.
+gdjs.registerFirstRuntimeSceneLoadedCallback(() => {
+  {
+    if (gdjs.BBTextRuntimeObject) {
+      gdjs.BBTextRuntimeObject.prototype.getString =
+        gdjs.BBTextRuntimeObject.prototype.getBBText;
+      gdjs.BBTextRuntimeObject.prototype.setString =
+        gdjs.BBTextRuntimeObject.prototype.setBBText;
+    }
+    if (gdjs.BitmapTextRuntimeObject) {
+      gdjs.BitmapTextRuntimeObject.prototype.getString =
+        gdjs.BitmapTextRuntimeObject.prototype.getText;
+      gdjs.BitmapTextRuntimeObject.prototype.setString =
+        gdjs.BitmapTextRuntimeObject.prototype.setText;
+      gdjs.BitmapTextRuntimeObject.prototype.getColor =
+        gdjs.BitmapTextRuntimeObject.prototype.getTint;
+      gdjs.BitmapTextRuntimeObject.prototype.setColor =
+        gdjs.BitmapTextRuntimeObject.prototype.setTint;
+    }
   }
-  if (gdjs.BitmapTextRuntimeObject) {
-    gdjs.BitmapTextRuntimeObject.prototype.getString =
-      gdjs.BitmapTextRuntimeObject.prototype.getText;
-    gdjs.BitmapTextRuntimeObject.prototype.setString =
-      gdjs.BitmapTextRuntimeObject.prototype.setText;
-    gdjs.BitmapTextRuntimeObject.prototype.getColor =
-      gdjs.BitmapTextRuntimeObject.prototype.getTint;
-    gdjs.BitmapTextRuntimeObject.prototype.setColor =
-      gdjs.BitmapTextRuntimeObject.prototype.setTint;
-  }
-}
+});
